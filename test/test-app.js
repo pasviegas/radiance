85a78035dc6f6c14ef894def8e495283a8c7325a
Pedro Antonio Souza Viegas <pedroasv@gmail.com>
2013-03-04 13:13:26 -0300

    add method to return json

--- test/test-app.js
+++ test/test-app.js
@@ -32,3 +32,17 @@ describe('GET /page/delete', function(){
       request(app).get('/page/delete/'+mockpage.name).expect(200, done);
   })    
 })
+
+describe('GET /page/:name/json', function(){
+  it('should return the page json', function(done){
+    mockpage.fakeRequests();
+    var newPage = page.create(mockpage);
+    newPage.save(function(){
+      request(app).get('/page/' + mockpage.name + "/json").expect(200)
+      .end(function(err, res){
+        assert.equal(mockpage.name, res.body.name)
+        done();
+      });  
+    })
+  })
+})
